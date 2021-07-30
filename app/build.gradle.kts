3302c50cb377295dfa3e05a2b383f938b8c8811a
Diego Cunha <diego.cunha@warren.com.br>
2021-07-30 11:06:55 -0300

    Start work with compose

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        applicationId = "com.br.diegocunha.mymovies"
+        minSdk = 21
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "BASE_URL", "\"https://api.themoviedb.org/3/\"")
+        buildConfigField("String", "API_KEY", "\"1f54bd990f1cdfb230adb312546d765d\"")
+        buildConfigField("String", "DEFAULT_LANGUAGE", "\"pt-BR\"")
+        buildConfigField("String", "DEFAULT_REGION", "\"BR\"")
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.android.material:material:1.4.0")
+
+
+    //Compose
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0")
+    implementation("androidx.compose.foundation:foundation:1.0.0")
+    implementation("androidx.compose.material:material:1.0.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
+    implementation("androidx.activity:activity-compose:1.3.0")
+
+    //Koin
+    implementation("io.insert-koin:koin-android:3.1.2")
+
+    //Network
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        maven { setUrl("https://dl.bintray.com/populov/maven") }
+        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://zendesk.jfrog.io/zendesk/repo") }
+        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
+        maven { setUrl("https://dl.bintray.com/apollographql/android") }
+    }
+    configurations.all {
+        resolutionStrategy.force("org.objenesis:objenesis:2.6")
+    }
+}
+
+plugins {
+    `kotlin-dsl`
+}
